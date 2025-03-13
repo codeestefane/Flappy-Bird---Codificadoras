727495947dd3b5f7779bea02782a2528cef94fdd
Estefane de Lima <codeestefane@gmail.com>
2025-03-13 19:20:52 -0300

    Versão sem sprite

--- main.js
+++ main.js
@@ -8,45 +8,43 @@ canvas.height = window.innerHeight;
 ctx.fillStyle = "#cb6ce6";
 ctx.fillRect(0, 0, canvas.width , canvas.height);
 
-let flappyBirdSprite = new Image();
 let birdAxisY = canvas.height/2 - 14;
-let birdAxisX = canvas.width/2;
+let birdAxisX = canvas.width/2 - 20;
 let posicaoOriginalY = birdAxisY;
-let direcao = "";
-
-flappyBirdSprite.src = "flappyBirdSprite.png";
+let pontuacao = -1;
 
 let start = false;
 
 function drawBird(i){
-    ctx.drawImage(flappyBirdSprite, 28 * i, 512 - 28, 23, 23, birdAxisX, birdAxisY, 96, 96);
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(birdAxisX, birdAxisY, 96, 96);
+    ctx.fillStyle = "white";
+    if(colisao) ctx.fillStyle = "#F00";
+    ctx.beginPath();
+    ctx.arc(birdAxisX, birdAxisY, 20, 0, 2 * Math.PI);
+    // ctx.stroke();
+    ctx.fill();
 }
 
 function drawBackground(){
-    ctx.fillStyle = "#E0BFB8";
+    ctx.fillStyle = "#ff66c4";
     
     for(let i = 0; i < 6; i++){
-        // ctx.beginPath();
-        // ctx.rect(canvas.width - incrementoBackground + 280 * i, canvas.height - alturaPipe[i], 80, alturaPipe[i]);
-        // ctx.rect(canvas.width + 280 * i - incrementoBackground, 0, 80, 580 - alturaPipe[i]);
-        // ctx.stroke();
-        // ctx.fill();
         if(posicaoPipe[i] < -80) posicaoPipe[i] = canvas.width;
-        ctx.drawImage(flappyBirdSprite, 84, 512 - 194, 26, 164, posicaoPipe[i], canvas.height - alturaPipe[i], 80, alturaPipe[i]);
-        ctx.drawImage(flappyBirdSprite, 56, 512 - 188, 26, 164, posicaoPipe[i], 0, 80, 580 - alturaPipe[i]);
-        posicaoPipe[i] = posicaoPipe[i] - 1;
+        ctx.beginPath();
+        ctx.rect(posicaoPipe[i], canvas.height - alturaPipe[i], 80, alturaPipe[i]);
+        ctx.rect(posicaoPipe[i], 0, 80, 550 - alturaPipe[i]);
+        ctx.stroke();
+        ctx.fill();
+        if(!colisao) posicaoPipe[i] = posicaoPipe[i] - 1;
     }
 }
 
-let incremento = 7;
+let incremento = 10;
 
 function effectFlyBird(){
-    if(birdAxisY > posicaoOriginalY + 7){
-        incremento = -7;
-    } else if(birdAxisY < posicaoOriginalY - 7){
-        incremento = 7;
+    if(birdAxisY > posicaoOriginalY + 10){
+        incremento = -10;
+    } else if(birdAxisY < posicaoOriginalY - 10){
+        incremento = 10;
     }
 
     birdAxisY += incremento;
@@ -54,64 +52,84 @@ function effectFlyBird(){
 
 let indexBird = 0;
 
-flappyBirdSprite.addEventListener('load', () => {
-    drawBird(0);
-});
-
-// setInterval(() => {
-//     if(indexBird === 3) indexBird = 0;
-//     ctx.fillStyle = "#cb6ce6";
-//     ctx.fillRect(0, 0, canvas.width , canvas.height);
-//     drawBird(indexBird);
-//     if(direcao === "") effectFlyBird();
-//     indexBird++;
-// }, 120);
-
 let tempo = 0;
 let tempoPulo = 0;
-
-let anima = setInterval(() => {
-    ctx.fillStyle = "#cb6ce6";
-    ctx.fillRect(0, 0, canvas.width , canvas.height);
-    if(indexBird === 3) indexBird = 0;
-    if(direcao === "cima"){
-        // direcao = ""
-        birdAxisY -= 2;
-    } else if(direcao === "baixo"){
-        birdAxisY += 2;
-        // direcao = ""
-    }
-
-    if(jump) jumpBird();
-    else if(start) birdAxisY += 4;
-
-    if(colisao){
-        birdAxisY = canvas.height - 26;
-        birdAxisX = 26;
-    }
-    
-    drawBird(indexBird);
-    
-    if(tempo % 120 === 0 && !start){
-        if(!start) effectFlyBird();
-        indexBird += 1;
-    }
-    
-    drawBackground();
-    tempo += 10;
-    verificaColisao();
-}, 10);
-
+let inicio;
+let anima;
+let colisao = false;
 let alturaPipe = []
 let posicaoPipe = []
+desenhaTelaInicio();
+
+function desenhaTelaInicio(){
+    pontuacao = -1;
+    start = false;
+    colisao = false;
+    birdAxisY = canvas.height/2 - 14;
+    inicio = setInterval(() => {
+        atualizaCenario();
+        ctx.fillStyle = "#cb6ce6";
+        ctx.fillRect(0, 0, canvas.width , canvas.height);
+        drawBird(indexBird);
+        effectFlyBird();
+        desenhaMensagemPressStart();
+    
+        if(start){
+            clearInterval(inicio);
+            iniciaJogo();
+        }
+    }, 60);
+}
+
+function desenhaMensagemPressStart(){
+    ctx.fillStyle = "#FFF";
+    ctx.font = "24px Arial";
+    ctx.fillText("Press space to start...", canvas.width/2 - 120, canvas.height/2 + 80);
+}
+
+function desenhaScore(color = "#FFF"){
+    ctx.fillStyle = color;
+    if(colisao) ctx.fillStyle = "red";
+    ctx.font = "24px Arial";
+    ctx.fillText("Score: " + pontuacao, canvas.width - 200, 30);
+}
+
+function iniciaJogo(){
+    pontuacao = 0;
+    anima = setInterval(() => {
+        ctx.fillStyle = "#cb6ce6";
+        ctx.fillRect(0, 0, canvas.width , canvas.height);
+    
+        if(jump && !colisao) jumpBird();
+        else if(start) {
+            if(colisao) birdAxisY += 6;
+            else birdAxisY += 4;
+        }
+
+        if(!colisao){
+            drawBird(indexBird);
+            drawBackground();
+        } else {
+            drawBackground();
+            drawBird(indexBird);
+        }
+
+        tempo += 10;
+        desenhaScore();
+        verificaColisao();
+
+        if(birdAxisY > canvas.height + 20){
+            clearInterval(anima);
+            desenhaTelaInicio();
+        }
+
+    }, 10);
+}
 
 function atualizaCenario(){
     for(let i = 0; i < 10; i++){
-        alturaPipe[i] = Math.round(Math.random() * (580 - 40) + 40);
+        alturaPipe[i] = Math.round(Math.random() * (550 - 40) + 40);
         posicaoPipe[i] = canvas.width + 260 * i;
-        // while(i !== 0 && !(alturaPipe[i - 1] - 40 <= alturaPipe[i] <= alturaPipe[i - 1] + 40)){
-        //     alturaPipe[i] = Math.round(Math.random() * (580 - 40) + 40);
-        // }
     }
 }
 
@@ -123,16 +141,16 @@ function jumpBird(){
     }
 }
 
-let colisao = false;
-
 function verificaColisao(){
     for(let i = 0; i < 6; i++){
-        if(posicaoPipe[i] <= birdAxisX + 80 && birdAxisX <= posicaoPipe[i] + 80 &&
-           (0 <= birdAxisY && birdAxisY <= 580 - alturaPipe[i])
+        if(posicaoPipe[i] < birdAxisX + 20 && birdAxisX - 20 < posicaoPipe[i] + 80 &&
+           (birdAxisY - 20 < 550 - alturaPipe[i] || (birdAxisY + 20 > canvas.height - alturaPipe[i]))
         ){
-                // console.log("aaaaaaa");
-                colisao = true;
-                clearInterval(anima);
+            colisao = true;
+        }
+
+        if(birdAxisX === posicaoPipe[i] + 80 ){
+            pontuacao += 1; 
         }
     }
 }
@@ -142,9 +160,7 @@ atualizaCenario();
 let jump = false;
 
 janelaUsuario.addEventListener("keypress", (evento) => {
-    if(evento.keyCode === 115) direcao = "baixo";
-    else if(evento.keyCode === 119) direcao = "cima";
-    else if(evento.keyCode === 32) {
+    if(evento.keyCode === 32) {
         start = true;
         jump = true;
         tempoPulo = tempo;
